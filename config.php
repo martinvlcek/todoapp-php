5f50b421248ca34d75ec30ee85dd9db27180123a
Martin Vlček <martin.vlcek93@gmail.com>
2019-12-13 13:55:05 +0100

    Create new connection to DB with PDO

--- config.php
+++ config.php
@@ -1,15 +1,14 @@
 <?php
 
-$db_host = '127.0.0.1';
-$db_user = 'root';
-$db_pass = 'root';
-$db_name = 'todoapp';
-$db_port = '3306';
+    $db_dns = 'mysql:host=localhost:3306;dbname=todoapp';
+    $db_user = 'root';
+    $db_pass = 'root';
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
-
-if (!$conn) {
-    die('Connection failed: ' . mysqli_connect_error());
-}
+    try {
+        $conn = new PDO($db_dns, $db_user, $db_pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        die('Connection failed: ' . $e->getMessage());
+    }
 
 ?>
