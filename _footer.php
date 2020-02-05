8caf92751552e39bfda874148ae5467667e80695
Martin Vlček <martin.vlcek93@gmail.com>
2020-02-05 13:04:56 +0100

    Update github link & add db dump

--- _footer.php
+++ _footer.php
@@ -1,11 +1,8 @@
 <footer class="mt-5">
     <div class="container">
         <p class="text-secondary">Created by Martin Vlcek.
-            <a href="https://github.com/martinvlcek" class="ml-2" target="_blank">
+            <a href="https://github.com/martinvlcek/todoapp-php" class="ml-2" target="_blank">
                 <i class="text-secondary fab fa-github"></i>
-            </a>
-            <a href="mailto:martin.vlcek93@gmail.com">
-                <i class="text-secondary far fa-envelope"></i>
             </a>
         </p>
     </div>
